ef68e871d27a5e74163a422d484a8369f9819396
Desh Iyer <desh.iyer@gmail.com>
2025-07-06 17:00:23 +0530

    Create rulesets as objects \& change file name

--- 1_basic_l_system.js
+++ 1_basic_l_system.js
@@ -8,6 +8,17 @@
 // Start out with our axiom
 var axiom = "A";
 
+// To model our ruleset, we can use JS objects
+var rule1 = {
+  a: "A", // Input
+  b: "AB" // Output
+}
+
+var rule2 = {
+  a: "B",
+  b: "A"
+}
+
 function setup() {
   createCanvas(400, 400);
 }

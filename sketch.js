758777571a1de4ef6a75bd979f3dabadc499e6c3
Desh Iyer <desh.iyer@gmail.com>
2025-07-08 22:38:32 +0530

    Define global scaling variables

--- sketch.js
+++ sketch.js
@@ -7,7 +7,20 @@ p5.js sketch to generate an L-system with the parameters:
     Axiom: F
     Rule: F -> FF+[+F-F-F]-[-F+F+F]
 
-This sketch also contains code to upscale the image and save a copy of the sketch's state.
+This sketch also contains code to upscale the image and save a copy of the sketch's state. Press the 'e' key to export a high-resolution snapshot of the canvas.
 
 */
 
+let canvas;
+let DIM = 1024; // Dimensions of the initial canvas
+let currentScale;
+let scaledCanvas;
+let outputScale = 8; // For an 8K image [8 * DIM (1024)]
+
+function setup() {
+
+}
+
+function draw() {
+
+}

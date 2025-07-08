192f62f7ea93000834fa6472e39bc435d4f09fbc
Desh Iyer <desh.iyer@gmail.com>
2025-07-08 22:33:20 +0530

    Update script

---
+++ sketch.js
@@ -0,0 +1,13 @@
+/* 
+
+Code by Desh Iyer.
+
+p5.js sketch to generate an L-system with the parameters:
+    Variables: {F, +, -, [, ]}
+    Axiom: F
+    Rule: F -> FF+[+F-F-F]-[-F+F+F]
+
+This sketch also contains code to upscale the image and save a copy of the sketch's state.
+
+*/
+
